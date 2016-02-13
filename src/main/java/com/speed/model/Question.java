db562ef16a19fe7c7510c69afd2a4bec6111a3da
Damian Grabowicz <damian.grabowicz@gmail.com>
2016-02-13 17:32:09 +0100

    początek

--- src/main/java/com/speed/model/Question.java
+++ src/main/java/com/speed/model/Question.java
@@ -3,5 +3,13 @@ package com.speed.model;
 /**
  * Created by piotr on 13.02.16.
  */
+
+//Przykładowe pytania
+//    Wybór ogólnej kategorii po uruchomieniu aplikacji
+//
+
+
+
 public class Question {
+
 }
