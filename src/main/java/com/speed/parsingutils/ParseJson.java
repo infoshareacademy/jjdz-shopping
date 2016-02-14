8a1f7e00a1d3e391da0acf20c930947bcbbf5faf
piotr <piotrpow@gmail.com>
2016-02-14 17:36:07 +0100

    Create class Parse

---
+++ src/main/java/com/speed/parsingutils/ParseJson.java
@@ -0,0 +1,7 @@
+package com.speed.parsingutils;
+
+/**
+ * Created by piotr on 13.02.16.
+ */
+public class ParseJson {
+}
--- src/main/java/com/speed/parsingutils/ParseXML.java
+++ src/main/java/com/speed/parsingutils/ParseXML.java
@@ -1,7 +1,19 @@
 package com.speed.parsingutils;
 
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 /**
  * Created by piotr on 13.02.16.
  */
 public class ParseXML {
+
+    public static void main(String[] args) {
+
+
+    }
+
 }
