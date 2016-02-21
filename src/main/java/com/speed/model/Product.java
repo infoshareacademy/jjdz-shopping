893d1f79669cb63ffc88f5f576f2d1e42bc51b9c
EwaW <ewaw@broadpark.no>
2016-02-21 14:54:16 +0100

    model klasy Product

--- src/main/java/com/speed/model/Product.java
+++ src/main/java/com/speed/model/Product.java
@@ -1,7 +1,30 @@
 package com.speed.model;
 
+
+
 /**
  * Created by ewaw on 13.02.16.
  */
 public class Product {
+    public String ProductName;
+    public String ProductNumber;
+
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public void setProductName(String productName) {
+        ProductName = productName;
+    }
+
+    public String getProductNumber() {
+        return ProductNumber;
+    }
+
+    public void setProductNumber(String productNumber) {
+        ProductNumber = productNumber;
+    }
+
+
 }
