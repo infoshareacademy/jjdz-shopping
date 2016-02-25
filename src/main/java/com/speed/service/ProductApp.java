1fb3e3cc96a9e6ac1875155f8f8f9489ff4706b5
EwaW <ewaw@broadpark.no>
2016-02-25 14:58:18 +0100

    String jako tymczasowy resultat z getProduct

--- src/main/java/com/speed/service/ProductApp.java
+++ src/main/java/com/speed/service/ProductApp.java
@@ -4,9 +4,14 @@ import com.google.zxing.BinaryBitmap;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.Result;
-import com.speed.model.Product;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+//import com.speed.model.Product;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -16,24 +21,41 @@ public class ProductApp {
 
     private Scanner sc = new Scanner(System.in);
 
-    public Product getProduct(){
+    public String /*Product*/ getProduct(){
         String fileName = getFilePath();
         BinaryBitmap myMap = GetBitMapfromFile(fileName);
         MultiFormatReader reader = new MultiFormatReader();
+        Result result = null;
         try {
-            Result result = reader.decode(myMap);
+            result = reader.decode(myMap);
         } catch (NotFoundException e) {
             e.printStackTrace();
         }
-        return null;
+        if (result == null) throw new AssertionError();
+
+        String gtin = result.getText();  //toString????
+
+        //druga czesc - wyciagniecie info z API
+
+        return gtin;
     }
 
     private BinaryBitmap GetBitMapfromFile(String fileName) {
-        File file = new File(fileName);
-        //      convert filobject to ???? > ???? > binarybitmap
+        File img = new File(fileName);
+        BufferedImage bufferedImage = null;
+        try {
+            bufferedImage = ImageIO.read(img);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        if (bufferedImage == null) {
+            throw new AssertionError();
+        }
 
-        return null;
+        HybridBinarizer hb = new HybridBinarizer(new BufferedImageLuminanceSource(bufferedImage));
+
+        return new BinaryBitmap(hb);
     }
 
 
