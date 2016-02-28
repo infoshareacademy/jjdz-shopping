83098b4512ccb9ef810c4f25c299e44af9d7ad73
Ewa Wierzbicka <ewaw@broadpark.no>
2016-02-28 16:40:23 +0100

    testy

---
+++ src/test/java/com/speed/ProductAppTest.java
@@ -0,0 +1,17 @@
+package com.speed;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ewaw on 28.02.16.
+ */
+public class ProductAppTest {
+
+    @Test
+    public void testGetProduct() throws Exception {
+
+    }
+}
