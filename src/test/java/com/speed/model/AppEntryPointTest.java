79c72805e34e6eb9046766fda49f541d801b81b1
Damian Grabowicz <damian.grabowicz@gmail.com>
2016-02-28 17:46:15 +0100

    JZSS-14 - dodano pierwszy test

---
+++ src/test/java/com/speed/model/AppEntryPointTest.java
@@ -0,0 +1,31 @@
+package com.speed.model;
+
+import org.junit.Test;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class AppEntryPointTest {
+
+    @Test
+    public void testSearchShops() throws Exception {
+
+        Shop shop = new Shop() {
+            @Override
+            public List<URI> search(TV tv) {
+                return Arrays.asList(
+                        URI.create("https://kjhkjh.kjhk.pl"),
+                        URI.create("https://kjhkjh.fsdfsdkjhk.pl")
+                );
+            }
+        };
+
+        AppEntryPoint aep = new AppEntryPoint(shop, System.out, Arrays.asList("1").iterator());
+
+        List<URI> uris = aep.searchShops();
+        assertThat(uris, org.hamcrest.Matchers.hasSize(2));
+    }
+}
