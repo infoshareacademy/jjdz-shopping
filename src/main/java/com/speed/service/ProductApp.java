94ae3e49c81cd2c03f1f41ad7a072a67e8fec1fb
EwaW <ewaw@broadpark.no>
2016-03-10 16:41:33 +0100

    kompletny prodductApp (czytanie zdjecia i zdobywanie informacji zwiazanej z GTIN)

--- src/main/java/com/speed/service/ProductApp.java
+++ src/main/java/com/speed/service/ProductApp.java
@@ -7,15 +7,16 @@ import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
 import com.speed.model.Product;
-import com.speed.model.Shop;
-//import com.speed.model.Product;
+import org.xml.sax.SAXException;
+
 
 import javax.imageio.ImageIO;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.stream.StreamSource;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.util.Iterator;
+import java.io.StringReader;
 import java.util.Scanner;
 
 /**
@@ -23,9 +24,7 @@ import java.util.Scanner;
  */
 public class ProductApp {
 
-    private Scanner sc = new Scanner(System.in);
-
-    public /*String */ Product getProduct() throws IOException {
+    public Product getProduct() throws ParserConfigurationException, SAXException {
 
         //decode picture to code
 
@@ -38,19 +37,36 @@ public class ProductApp {
         } catch (NotFoundException e) {
             e.printStackTrace();
         }
-        if (result == null) throw new AssertionError();
+        if (result == null) {
+            throw new AssertionError();
+        }
 
         Product product = new Product();
+
         String gtin = result.getText();  //toString????
+//        String gtin = "05900352000602";
         product.setProductNumber(gtin);
 
         //druga czesc - wyciagniecie info z API
         RestAPIProvider r = new RestAPIProvider("http://api3.produktywsieci.pl/PublicService.svc/rest/xml/GetProductByGTIN");
+        String APIkey = "bd9HdzJMoMvpkn@JuVfp@Czozrpc_UM4gvmn";
 
-        r.addParam("gs1Key","KiedysDodam");
-        r.addParam("gtin",result.getText());
+        r.addParam("gs1Key", APIkey);
+        r.addParam("gtin", gtin);
 
-        String stringWithRestResponse = r.sendRestApiRequest();
+        String stringWithRestResponse = null;
+        try {
+            stringWithRestResponse = r.sendRestApiRequest();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ReadXMLFile rxm = new ReadXMLFile();
+
+        try {
+            product = rxm.parseXML(stringWithRestResponse, product);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return product;
     }
@@ -67,7 +83,6 @@ public class ProductApp {
         if (bufferedImage == null) {
             throw new AssertionError();
         }
-
         HybridBinarizer hb = new HybridBinarizer(new BufferedImageLuminanceSource(bufferedImage));
 
         return new BinaryBitmap(hb);
@@ -75,11 +90,7 @@ public class ProductApp {
 
 
     public String getFilePath() {
-        System.out.println("Podaj sciezke pliku: ");
-        return getUserInput();
-    }
-
-    public String getUserInput() {
-        return sc.nextLine().trim();
+        return "/home/ewa/Documents/Programming/IdeaProjects/jjdz-shopping/src/main/java/com/speed/service/IMG_0528.JPG";
     }
 }
+
