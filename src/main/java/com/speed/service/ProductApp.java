f9f2b58ebf4ef5c9013736c7eb1acbf8392d05cd
Ewa Wierzbicka <ewaw@broadpark.no>
2016-03-12 12:44:24 +0100

    JZSS-2 (exeptions)

--- src/main/java/com/speed/service/ProductApp.java
+++ src/main/java/com/speed/service/ProductApp.java
@@ -9,42 +9,39 @@ import com.google.zxing.common.HybridBinarizer;
 import com.speed.model.Product;
 import org.xml.sax.SAXException;
 
-
 import javax.imageio.ImageIO;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.stream.StreamSource;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.StringReader;
-import java.util.Scanner;
 
 /**
  * Created by ewa on 2/21/16.
  */
 public class ProductApp {
 
-    public Product getProduct() throws ParserConfigurationException, SAXException {
+    public Product getProduct() throws IOException {
 
         //decode picture to code
 
         String fileName = getFilePath();
-        BinaryBitmap myMap = GetBitMapfromFile(fileName);
-        MultiFormatReader reader = new MultiFormatReader();
-        Result result = null;
+        Result result;
         try {
+            BinaryBitmap myMap = GetBitMapfromFile(fileName);
+            MultiFormatReader reader = new MultiFormatReader();
             result = reader.decode(myMap);
-        } catch (NotFoundException e) {
+        }catch (IOException  | NotFoundException e) {
             e.printStackTrace();
+            throw new IOException("Error during reading and parsing from file. Reason: " + e.getMessage(), e);
         }
+
         if (result == null) {
             throw new AssertionError();
         }
 
         Product product = new Product();
-
         String gtin = result.getText();  //toString????
-//        String gtin = "05900352000602";
+        //        String gtin = "05900352000602";
         product.setProductNumber(gtin);
 
         //druga czesc - wyciagniecie info z API
@@ -54,31 +51,22 @@ public class ProductApp {
         r.addParam("gs1Key", APIkey);
         r.addParam("gtin", gtin);
 
-        String stringWithRestResponse = null;
         try {
-            stringWithRestResponse = r.sendRestApiRequest();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ReadXMLFile rxm = new ReadXMLFile();
-
-        try {
+            String stringWithRestResponse = r.sendRestApiRequest();
+            ReadXMLFile rxm = new ReadXMLFile();
             product = rxm.parseXML(stringWithRestResponse, product);
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException | SAXException | ParserConfigurationException e) {
+            throw new IOException("Error during getting product information. Reason: "+ e.getMessage(),e);
         }
 
         return product;
+
     }
 
-    private BinaryBitmap GetBitMapfromFile(String fileName) {
+    private BinaryBitmap GetBitMapfromFile(String fileName) throws IOException {
         File img = new File(fileName);
         BufferedImage bufferedImage = null;
-        try {
-            bufferedImage = ImageIO.read(img);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        bufferedImage = ImageIO.read(img);
 
         if (bufferedImage == null) {
             throw new AssertionError();
