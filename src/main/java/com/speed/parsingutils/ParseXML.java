1eb6a940928436956bf39356986e50090930bbda
piotr <piotrpow@gmail.com>
2016-03-12 19:01:44 +0100

    Create class Parse

--- src/main/java/com/speed/parsingutils/ParseXML.java
+++ src/main/java/com/speed/parsingutils/ParseXML.java
@@ -1,11 +1,15 @@
 package com.speed.parsingutils;
 
+import javax.xml.namespace.QName;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.StartElement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by piotr on 13.02.16.
@@ -13,79 +17,50 @@ import java.util.List;
 public class ParseXML {
 
     public static void main(String[] args) throws XMLStreamException {
-
-        Allegro allegro = null;
-        List<Allegro> allegroList = null;
-        String tagContent = null;
         XMLInputFactory factory = XMLInputFactory.newFactory();
-        XMLStreamReader parsner = factory.createXMLStreamReader(ClassLoader.getSystemResourceAsStream("files/allegro1.xml"));
+        XMLStreamReader parser = factory.createXMLStreamReader(ClassLoader.getSystemResourceAsStream("files/allegro.xml"));
 
-        while (parsner.hasNext()) {
+        boolean inItem = false;
+        String currentTag = "";
 
-            int event = parsner.next();
+        while (parser.hasNext()) {
+            int event = parser.next();
             switch (event) {
                 case XMLStreamConstants.START_ELEMENT:
-                    if ("id".equals(parsner.getLocalName())) {
-                        allegro = new Allegro();
-                        allegro.id = parsner.getAttributeValue(0);
+                    String tagName = parser.getLocalName();
+                    if (tagName.equals("item")) {
+                        inItem = true;
+                        int attrCount = parser.getAttributeCount();
+                        System.out.println(attrCount);
+                        //zaczynamy zbierac
                     }
-
-                    if ("allegroList".equals(parsner.getLocalName())) {
-                        allegroList = new ArrayList<Allegro>();
+                    if (inItem) {
+                        currentTag = tagName;
                     }
                     break;
+
                 case XMLStreamConstants.CHARACTERS:
-                    tagContent = parsner.getText().trim();
-                    break;
-
-                case XMLStreamConstants.END_ELEMENT:
-                    String s = parsner.getLocalName();
-                    if (s.equals("employee")) {
-                        allegroList.add(allegro);
-
-                    } else if (s.equals("catName")) {
-                        allegro.catName = tagContent;
-                        break;
-
-                    } else if (s.equals("catParent")) {
-                        allegro.catParent = tagContent;
-                        break;
-
-                    } else if (s.equals("catPosition")) {
-                        allegro.catPosition = tagContent;
-                        break;
+                    if (inItem) {
+                        String text = parser.getText().trim();
+                        if (!text.isEmpty()) {
+                            System.out.println(currentTag + " - " + text);
+                            //zbieramy
+                        }
 
                     }
                     break;
-
-                case XMLStreamConstants.START_DOCUMENT:
-                    allegroList = new ArrayList<Allegro>();
+                case XMLStreamConstants.END_ELEMENT:
+                    if (parser.getLocalName().equals("item")) {
+                        //zebrane uzywamy do stworzenia kategorii
+                        inItem = false;
+                    }
                     break;
+                case XMLStreamConstants.ATTRIBUTE:
+
+                    break;
+
             }
+        }
 
-        }
-        for (Allegro alg : allegroList) {
-            System.out.println("alg");
-        }
     }
 }
-
-class Allegro {
-    String id;
-    String catName;
-    String catParent;
-    String catPosition;
-
-
-    @Override
-    public String toString() {
-        return "Allegro{" +
-                "catId='" + id + '\'' +
-                ", catName='" + catName + '\'' +
-                ", catParent='" + catParent + '\'' +
-                ", catPosition='" + catPosition + '\'' +
-                '}';
-    }
-}
-
-
