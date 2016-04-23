117dcd46f04b266ab9bf83ad49d0a61d7e2c5d5e
Damian Grabowicz <damian.grabowicz@gmail.com>
2016-04-23 17:59:37 +0200

    InputSearchByProductFormServlet updated

--- src/main/java/com/speed/service/InputSearchByProductFormServlet.java
+++ src/main/java/com/speed/service/InputSearchByProductFormServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "FrontEnd/InputSearchByProductForm")
+@WebServlet(urlPatterns = "FrontEnd/SearchByProductServlet")
 public class InputSearchByProductFormServlet extends HttpServlet {
 
     @EJB
@@ -21,12 +21,10 @@ public class InputSearchByProductFormServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
-
         String searchedProduct = req.getParameter("searchedProduct");
 
         String result = categorySearch.searchCategoryByGivenProduct(searchedProduct);
-        System.out.println("aaaaa");
+        System.out.println(result);
 
         req.setAttribute("result", result);
 
@@ -36,11 +34,11 @@ public class InputSearchByProductFormServlet extends HttpServlet {
 
     }
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
-        RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
-        dispatcher.forward(req, resp);
-    }
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//
+//
+//        RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
+//        dispatcher.forward(req, resp);
+//    }
 }
