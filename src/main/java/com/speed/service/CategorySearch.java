06708d2b91381244fa55662f8911641206b2871b
Damian Grabowicz <damian.grabowicz@gmail.com>
2016-04-24 15:58:29 +0200

    Class modified in order to search through the list parsedCategories being result of allegro.xml parsing instead of hard coded categoryList

--- src/main/java/com/speed/service/CategorySearch.java
+++ src/main/java/com/speed/service/CategorySearch.java
@@ -1,8 +1,10 @@
 package com.speed.service;
 
 import com.speed.model.Category;
+import com.speed.parsingutils.ParseXML;
 
 import javax.ejb.Stateless;
+import javax.xml.stream.XMLStreamException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,51 +15,28 @@ import java.util.List;
 @Stateless
 public class CategorySearch {
 
-    private List<Category> categories;
-    private String foundCategory = "";
+    private List<Category> parsedCategories;
 
-    public CategorySearch() {
-        this.categories = new ArrayList<Category>();
+    public CategorySearch() throws XMLStreamException {
+        this.parsedCategories = new ParseXML().parsStax("files/allegro.xml");
 
-        categories.add(new Category(26013, 0, "Rowery"));
-        categories.add(new Category(98553, 0, "Telewizory"));
-        categories.add(new Category(64477, 0, "Odzież"));
-        categories.add(new Category(12345, 64477, "Spodnie"));
-        categories.add(new Category(22334, 26013, "Rowery miejskie"));
     }
 
-    public List<Category> getCategories() {
-        return categories;
+    public List<Category> getParsedCategories() {
+        return parsedCategories;
     }
 
-    public void setCategories(List<Category> categories) {
-        this.categories = categories;
-    }
+    public List<Category> searchCategoryByGivenProduct(String searchedProduct) {
 
-    public String getFoundCategory() {
-        return foundCategory;
-    }
+        List<Category> foundCategories = new ArrayList<>();
 
-    public void setFoundCategory(String foundCategory) {
-        this.foundCategory = foundCategory;
-    }
-
-    public StringBuilder searchCategoryByGivenProduct(String searchedProduct) {
-
-        setFoundCategory("");
-        StringBuilder builder = new StringBuilder();
-        for (Category cat : categories) {
+        for (Category cat : parsedCategories) {
             if (cat.getCatName().toLowerCase().contains(searchedProduct.toLowerCase())) {
-                builder.append("Product can be found in category: " + cat.getCatName() + "<br>");
-
-//                    TODO: insert to database the name of found category instead of below setFoundCategory
-                setFoundCategory(cat.getCatName());
+                foundCategories.add(cat);
             }
         }
-        if (getFoundCategory().equals("")) {
-            builder.append("No category found for product: " + searchedProduct);
-        }
-        return builder;
+
+        return foundCategories;
     }
 }
 
